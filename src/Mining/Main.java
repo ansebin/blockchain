053c01af4dbac5308193fa1ae447817885dc4e23
ansebin <ansebin98@github.com>
2022-06-16 15:58:49 +0900

    first commit

---
+++ src/Mining/Main.java
@@ -0,0 +1,15 @@
+package Mining;
+
+public class Main {
+	public static void main(String[] args) {
+		Block block1 = new Block("kom","0");
+		block1.mine(3);
+		
+		Block block2 = new Block("dong","0");
+		block2.mine(3);
+		
+		Block block3 = new Block("heyon","0");
+		block2.mine(3);
+		
+	}
+}
